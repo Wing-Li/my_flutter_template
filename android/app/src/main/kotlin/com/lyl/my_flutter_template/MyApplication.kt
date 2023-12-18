bed9dda4e13b4a328f854aaa9d411a9874030081
Wing_Li <liyunliang@boo.chat>
2023-12-18 17:47:51 +0800

    为 Android 增加 multidex 库

---
+++ android/app/src/main/kotlin/com/lyl/my_flutter_template/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.lyl.my_flutter_template
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+
+class MyApplication : MultiDexApplication() {
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
