86e9a49e13089d125ac9282391fbc9210b637a26
Wing_Li <iamlyl@foxmail.com>
2024-01-23 14:18:52 +0800

    提取 js 方法

--- lib/page/message/message_page.dart
+++ lib/page/message/message_page.dart
@@ -1,21 +1,11 @@
-import 'dart:convert';
-import 'dart:js' as js;
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/instance_manager.dart';
-import 'package:js/js.dart';
-import 'package:js/js_util.dart';
 import 'package:my_flutter_template/page/base_state.dart';
 import 'package:my_flutter_template/page/message/message_controller.dart';
-import 'package:my_flutter_template/page/web/my_webview_page.dart';
 import 'package:my_flutter_template/res/my_styles.dart';
-import 'package:my_flutter_template/utils/my_utils.dart';
-
-@JS()
-external getMyAddress();
 
 class MessagePage extends StatefulWidget {
   final String title;
@@ -48,87 +38,10 @@ class _MessagePageState extends BaseState<MessagePage> {
   buildColumn() {
     if (widget.title == "首页") {
       return buildHome();
-    } else if (widget.title == "信息") {
-      return buildMine();
     } else {
       return Text(widget.title, style: MyTextStyles.textWhite(16));
     }
   }
-
-  void toastMsg(String msg) {
-    print("address: $msg");
-    MyUtils.showToast(msg);
-  }
-
-  Widget buildMine() {
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextButton(
-            onPressed: () {
-              // 给 JS 提供一个方法
-              js.context["toastMsg"] = toastMsg;
-
-              // 调用 JS 方法
-              var personInfo = js.context.callMethod('getPersonInfo');
-              print("getAddress is $personInfo");
-            },
-            child: Text("js2PersonInfo"),
-          ),
-          SizedBox(height: 20),
-          TextButton(
-            onPressed: () async {
-              // 调用Js的方法
-              try {
-                var promise = getMyAddress();
-                var result = await promiseToFuture(promise); // 第三个坑
-                print("getAddress is $result");
-              } catch (e) {
-                print("getDeviceInfo错误信息是 $e");
-              }
-            },
-            child: Text("getMyAddress"),
-          ),
-          SizedBox(height: 20),
-        ],
-      ),
-    );
-  }
-
-  // Widget buildMy() {
-  //   return Container(
-  //     child: InAppWebView(
-  //       initialUrlRequest: URLRequest(url: Uri.tryParse('https://sugars.zone/')),
-  //       onWebViewCreated: (controller) async {
-  //         // register a JavaScript handler with name "myHandlerName"
-  //         // controller.addJavaScriptHandler(
-  //         //     handlerName: 'handleFreeMint',
-  //         //     callback: (args) {
-  //         //       // print arguments coming from the JavaScript side!
-  //         //       print(args);
-  //         //
-  //         //       // return data to the JavaScript side!
-  //         //       return {'bar': 'bar_value', 'baz': 'baz_value'};
-  //         //     });
-  //
-  //         await controller.addWebMessageListener(WebMessageListener(
-  //           jsObjectName: "handleFreeMint",
-  //           onPostMessage: (message, sourceOrigin, isMainFrame, replyProxy) {
-  //             MyUtils.log("message: $message");
-  //             MyUtils.log("sourceOrigin: $sourceOrigin");
-  //             // do something about message, sourceOrigin and isMainFrame.
-  //             replyProxy.postMessage("Got it!");
-  //           },
-  //         ));
-  //       },
-  //       onLoadStop: (controller, url) async {
-  //         var result = await controller.evaluateJavascript(source: "foo + bar");
-  //         print(result); // 51
-  //       },
-  //     ),
-  //   );
-  // }
 
   Widget buildHome() {
     return Column(
