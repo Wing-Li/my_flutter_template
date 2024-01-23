c2c2acbf35e9b45d12ec5ad2f9d948d0bfc61a4d
Wing_Li <iamlyl@foxmail.com>
2024-01-23 14:36:40 +0800

    隐藏 js

--- lib/plugin/web_js.dart
+++ lib/plugin/web_js.dart
@@ -1,34 +1,35 @@
-import 'package:js/js.dart';
-import 'package:js/js_util.dart';
-import 'dart:js' as js;
-import 'package:my_flutter_template/utils/my_utils.dart';
-
-@JS()
-external getMyAddress();
-
-class WebJs {
-  void toastMsg(String msg) {
-    print("address: $msg");
-    MyUtils.showToast(msg);
-  }
-
-  getPersonInfo() {
-    // 给 JS 提供一个方法
-    js.context["toastMsg"] = toastMsg;
-
-    // 调用 JS 方法
-    var personInfo = js.context.callMethod('getPersonInfo');
-    print("getAddress is $personInfo");
-  }
-
-  getMyAddress() async {
-    // 调用Js的方法
-    try {
-      var promise = getMyAddress();
-      var result = await promiseToFuture(promise); // 第三个坑
-      print("getAddress is $result");
-    } catch (e) {
-      print("getDeviceInfo错误信息是 $e");
-    }
-  }
-}
+// import 'package:js/js.dart';
+// import 'package:js/js_util.dart';
+// import 'dart:js' as js;
+// import 'package:my_flutter_template/utils/my_utils.dart';
+//
+// @JS()
+// external getMyAddress();
+//
+// /// js: ^0.6.7
+// class WebJs {
+//   void toastMsg(String msg) {
+//     print("address: $msg");
+//     MyUtils.showToast(msg);
+//   }
+//
+//   getPersonInfo() {
+//     // 给 JS 提供一个方法
+//     js.context["toastMsg"] = toastMsg;
+//
+//     // 调用 JS 方法
+//     var personInfo = js.context.callMethod('getPersonInfo');
+//     print("getAddress is $personInfo");
+//   }
+//
+//   getMyAddress() async {
+//     // 调用Js的方法
+//     try {
+//       var promise = getMyAddress();
+//       var result = await promiseToFuture(promise); // 第三个坑
+//       print("getAddress is $result");
+//     } catch (e) {
+//       print("getDeviceInfo错误信息是 $e");
+//     }
+//   }
+// }
